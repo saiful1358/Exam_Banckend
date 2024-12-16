50fab0a5886ee534a215c3ac23d52cc5cd3feb96
saiful1358 <saifuddinmahmud74@gmail.com>
2024-12-16 13:01:04 +0600

    Create UserModel.js

---
+++ app/model/UserModel.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+    
+    {
+        email: {type: String, unique: true, required: true},
+        firstName: {type: String, required: true},
+        lastName: {type: String, required: true},
+        mobile: {type: String, required: true},
+        password: {type: String, required: true},
+        otp:{type: String, default: 0},
+            NIDNumber : {type: String, required: true},
+
+    },
+    { timestamps: true , versionKey: false}
+
+)
+const UsersModel = mongoose.model("users", userSchema);
+export default UsersModel
